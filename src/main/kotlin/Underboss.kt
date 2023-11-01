38f829057b5f120cb5c64430e979d50e115b1f2c
mahmoudalame <alamemany27@gmail.com>
2023-11-01 11:30:10 +0100

    aktuelles Project

---
+++ src/main/kotlin/Underboss.kt
@@ -0,0 +1,29 @@
+
+class UnderBoss(name: String, maxHp: Int) : Opponent(name, maxHp) {
+    init {
+        actions = listOf(
+            Action("Dark Magic", 30),
+            Action("Shadow Strike", 60),
+        )
+    }
+    override fun doSomeAction(team: Team) {
+        val randomAction: Action = actions.random()
+        if (randomAction.name == "Dark Magic") {
+            team.heroes.forEach { hero ->
+                hero.takeDamage(randomAction.damage)
+            }
+        } else if (randomAction.name == "Shadow Strike") {
+            val randomHero = team.heroes.random()
+            randomHero.takeDamage(randomAction.damage)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
